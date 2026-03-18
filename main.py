16d46c575d49fc946531984345b9bd3d06f5a710
Guilherme <djcomerlato042@gmail.com>
2026-03-17 23:32:41 -0300

    codigo final funcionando

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template_string
-import os
 
 app = Flask(__name__)
 
@@ -82,8 +81,3 @@ html = """
 @app.route("/")
 def home():
     return render_template_string(html)
-
-# 🔥 ESSA PARTE RESOLVE O RENDER
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
